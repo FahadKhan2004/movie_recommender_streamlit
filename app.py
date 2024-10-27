e2cbf13ac6489f8500943e27fd4a6420fc6c2b81
FahadKhan2004 <mohammedfahadk81@gmail.com>
2024-10-27 19:53:50 +0530

    change 1 removecommentandupdateheading

--- app.py
+++ app.py
@@ -33,28 +33,9 @@ st.title('Movie Recommender System')
 
 #select box
 selected_movie_name=st.selectbox(
-    'How ',
+    'Search for a Movie:',
 movies['title'].values)
 
-'''if st.button('Recommend'):
-    names, posters=recommend(selected_movie_name)
-
-    col1, col2, col3, col4, col5 =st.columns(5)
-    with col1:
-        st.header(names[0])
-        st.image(posters[0])
-    with col2:
-        st.header(names[1])
-        st.image(posters[1])
-    with col3:
-        st.header(names[2])
-        st.image(posters[2])
-    with col4:
-        st.header(names[3])
-        st.image(posters[3])
-    with col5:
-        st.header(names[4])
-        st.image(posters[4])'''
 if st.button('Recommend'):
     names, posters = recommend(selected_movie_name)
 
